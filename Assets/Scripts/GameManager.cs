3329a20ada38080564fee62af230e93ef0f12cf9
loz <lauriej1316@gmail.com>
2024-01-04 10:01:34 +0000

    Singleton and GameManager patterns

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Use this as an entry point for your game. Keep a reference to core game systems here.
+/// </summary>
+[DefaultExecutionOrder(-9999)]
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance {
+        get {
+#if UNITY_EDITOR
+
+            if (_instance == null)
+            {
+                //in editor, we can start any scene to test, so we are not sure the game manager will have been
+                //created by the first scene starting the game. So we load it manually. This check is useless in
+                //player build as the 1st scene will have created the GameManager so it will always exists.
+                Instantiate(Resources.Load<GameManager>("GameManager"));
+            }
+#endif
+            return _instance;
+        }
+    }
+
+    // references to all the core systems here
+    //public TerrainManager Terrain { get; set; }
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+}
---
+++ Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+
+    public static T Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (Instance != null)
+        {
+            DestroyImmediate(this);
+            return;
+        }
+        Instance = GetComponent<T>();
+    }
+}
